be3d9fd1263d421e0197dbf300febb62008180ff
Elijah <elijah.jegede@mygeorgian.ca>
2025-12-12 22:46:59 -0500

    Fixed the nav menu not working when resizing

--- project-1/script.js
+++ project-1/script.js
@@ -1,5 +1,6 @@
 menuIcon = document.getElementById("menu-icon");
 
+// Toggle navigation links on menu icon click
 menuIcon.addEventListener("click", () => {
     const navLinks = document.querySelector("nav ul");
     if (navLinks.style.display === "flex") {
@@ -7,6 +8,14 @@ menuIcon.addEventListener("click", () => {
     }
     else {
         navLinks.style.display = "flex";
-        navLinks.style.flexDirection = "column";
+    }
+});
+
+// Ensure nav links are shown when resizing to larger screens
+window.addEventListener("resize", () => {
+    menuIcon = document.getElementById("menu-icon");
+    const navLinks = document.querySelector("nav ul");
+    if (window.innerWidth >= 776) {
+        navLinks.style.display = "flex";
     }
 });
